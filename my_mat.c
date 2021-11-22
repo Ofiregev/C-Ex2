c9a6d4829265815c0b9ab721f9da1771ad55a5ca
OFIR REGEV <92684730+Ofiregev@users.noreply.github.com>
2021-11-22 16:06:02 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -12,10 +12,14 @@ void findShortest()
         {
             for (j = 0; j < n; j++)
             {
-                if(matrix[i][j]==0){
-                     matrix[i][j]=maxint;
+                if(i!=j){
+                
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][j] = maxint;
                 }
                 matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+            }
             }
         }
     }
@@ -29,6 +33,7 @@ int min(int a, int b)
 
 void input()
 {
+    
 
     int e, r;
     for (e = 0; e < n; e++)
@@ -36,34 +41,26 @@ void input()
         for (r = 0; r < n; r++)
         {
             scanf("%d", &matrix[e][r]);
-            // for(e = 0; e < n; e++){
-            //     for(r = e+1; r <n;r++){
-
-            //     printf("Please enter element (%d, %d): ", e, r);
-
-            //     scanf("%d", &input);
-            //     matrix[e][r] = input;
-            //     matrix[r][e]=input;
         }
     }
     findShortest();
 
-    // int p;
-    // int q;
-    // for (p = 0; p < n; p++)
-    // {
-    //     for (q = 0; q < n; ++q)
-    //     {
-    //         printf("%d, ", matrix[p][q]);
-    //     }
-    //     printf("\n");
-    // }
+    int p;
+    int q;
+    for (p = 0; p < n; p++)
+    {
+        for (q = 0; q < n; ++q)
+        {
+            printf("%d, ", matrix[p][q]);
+        }
+        printf("\n");
+    }
 }
 
 void hasPath(int i, int j)
 {
 
-    if (matrix[i][j] != 1000)
+    if (matrix[i][j] != 0&&matrix[i][j] != 1000)
     {
         printf("True\n");
     }
@@ -74,7 +71,7 @@ void hasPath(int i, int j)
 }
 void shortestPath(int i, int j)
 {
-    if (matrix[i][j] != 1000)
+    if (matrix[i][j] != 0&&matrix[i][j]!=maxint)
     {
         printf("%d\n", matrix[i][j]);
     }
