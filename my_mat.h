c2355c87c3b1e18aab734caa0375a8583f42c452
OFIR REGEV <92684730+Ofiregev@users.noreply.github.com>
2021-11-22 14:50:00 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,5 +1,5 @@
-int input(double matrix[10][10]);
-int hasPath(double matrix[10][10],int i,int j);
-int shortestPath(double matrix[10][10],int i,int j);
-int findShortest(double matrix [10][10]);
-double min(double a, double b);
+void input();
+void hasPath(int i,int j);
+void shortestPath(int i,int j);
+void findShortest();
+int min(int a, int b);
